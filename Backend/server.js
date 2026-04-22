62ac0d442c9150468fa091cb980830311b21c32d
khounvisal <khoun.visal25@kit.edu.kh>
2026-04-23 00:17:51 +0700

    update code

--- Backend/server.js
+++ Backend/server.js
@@ -12,12 +12,14 @@ app.use(express.json());
 const userRoutes = require("./routes/userRoutes");
 app.use("/api/users", userRoutes);
 
-// database
-mongoose.connect("mongodb://localhost:27017/")
+// database (USE ENV VARIABLE)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-// server
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// port fix for Railway
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
